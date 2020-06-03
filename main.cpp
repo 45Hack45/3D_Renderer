faf4159c2be9dbcc73770687c0994252fd38f7ec
45Hack45 <45haha45@gmail.com>
2020-06-04 00:25:43 +0200

    test render  squere

--- main.cpp
+++ main.cpp
@@ -96,25 +96,40 @@ void test_render_triangle(GLFWwindow* window) {
 	// ------------------------------------------------------------------
 
 	GLfloat vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	0.5f,  0.5f, 0.0f,  // top right
+	0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+	};
+	
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);//Create buffer
+	glGenBuffers(1, &VBO);//Create vertex buffer
+	glGenBuffers(1, &EBO);//create triangle buffer
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);//Bind buffer
 	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices), vertices, GL_STATIC_DRAW);//Send data to gpu
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//Bind buffer
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//Send data to gpu
+
 	//set the vertex attributes pointer. Indicate to OpenGL how to read the data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);//unbinding VBO
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 	glBindVertexArray(0);//unbinding VAO
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -130,11 +145,17 @@ void test_render_triangle(GLFWwindow* window) {
 
 		glUseProgram(shaderProgram);//Bind shader program
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);//Update screen
 		glfwPollEvents();//check events
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 }
 
 int main() {
@@ -181,7 +202,6 @@ int main() {
 		//Clearing screen
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
 
 
 		glfwSwapBuffers(window);//Update screen
