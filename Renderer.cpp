fd60866626b35eba1c2e7d3aa50f4179cbaf9019
Hack45 <45haha45@gmail.com>
2021-07-13 18:36:01 +0200

    Revert "WIP: Initial development"
    This reverts commit 35cf2c281c5ad6a76db35d4e704ded060087dfde.

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,6 +1,5 @@
 #include "Renderer.h"
 #include "logger.h"
-#include "ShaderManager.hpp"
 
 namespace Engine
 {
@@ -62,98 +61,6 @@ namespace Engine
 
 	float Renderer::getWindowAspectRatio()const {
 		return m_window_size_X / (1 + m_window_size_Y);
-	}
-
-	bool renderPassSortPred(const IRenderPass* a, const IRenderPass* b) {
-		return a->getRenderOrder() < b->getRenderOrder();
-	}
-
-	void Renderer::Render()
-	{
-		const FrameBuffer* aux = nullptr;
-
-		for (int i = 0; i < renderPasses.size(); i++)
-		{
-			aux = renderPasses[i]->RenderPass(aux);
-		}
-
-		
-
-		for (int i = 0; i < renderPasses.size(); i++)
-		{
-			renderPasses[i]->RenderPassDebugGUI();
-		}
-	}
-
-	void Renderer::addRenderPass(IRenderPass* renderPass)
-	{
-		renderPasses.push_back(renderPass);
-
-		//Sort in ascending order
-		std::sort(renderPasses.begin(), renderPasses.end(), renderPassSortPred);
-	}
-
-	void Renderer::drawFullScreenQuad(unsigned int textureID, unsigned int textureAtachment, bool useFullScreenQuadShader) {
-
-		static float vertices[] = {
-			//Position		
-			 1,  1, 0.0f,	1, 1,	// top right
-			 1, -1, 0.0f,	1, 0,	// bottom right
-			-1, -1, 0.0f,	0, 0,	// bottom left
-			-1,  1, 0.0f,	0, 1	// top left 
-		};
-		static unsigned int indices[] = {  // note that we start from 0!
-			2,0,3,
-			2,1,0
-		};
-		static unsigned int VBO, VAO, EBO;
-
-		static bool initialized = false;
-
-		if (!initialized) {
-			//create buffers
-			glGenVertexArrays(1, &VAO);
-			glGenBuffers(1, &VBO);
-			glGenBuffers(1, &EBO);
-			// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-			glBindVertexArray(VAO);
-
-			glBindBuffer(GL_ARRAY_BUFFER, VBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-			// position attribute
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-			glEnableVertexAttribArray(0);
-			// uv attribute
-			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-			glEnableVertexAttribArray(1);
-
-			// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-			glBindVertexArray(0);
-		}
-
-
-		if (useFullScreenQuadShader) {
-			Shader* sh = ShaderManager::Instance()->getShader("FullScreenQuadShader");
-			sh->bind();
-		}
-
-		//bind texture
-		glActiveTexture(GL_TEXTURE0 + textureAtachment); // activate the texture unit first before binding texture
-		glBindTexture(GL_TEXTURE_2D, textureID);
-
-
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		//glDeleteVertexArrays(1, &VAO);
-		//glDeleteBuffers(1, &VBO);
-		//glDeleteBuffers(1, &EBO);
 	}
 }
 
--- Renderer.h
+++ Renderer.h
@@ -3,54 +3,28 @@
 #ifndef RENDERER_H_
 #define RENDERER_H_
 
-#include <vector>
-
 #include "glad.h"
 #include "glfw3.h"
 
 #include "logger.h"
-#include "Color.h"
 
-#include "FrameBuffer.h"
+#include "Color.h"
 
 namespace Engine
 {
-	class IRenderPass
-	{
-	public:
-		static const unsigned int RenderPassOrder_Opaque =		4;
-		static const unsigned int RenderPassOrder_Transparent = 8;
-		static const unsigned int RenderPassOrder_FullScreen =	12;
-
-		//Draw.
-		//Optional input framebuffer
-		//Optional output framebuffer
-		virtual const FrameBuffer* RenderPass(const FrameBuffer* in_fbo) = 0;
-
-		virtual void RenderPassDebugGUI() {};
-
-		//Returns render pass render order. Lower value are rendered first
-		virtual unsigned int getRenderOrder() const = 0;
-	};
-
 	class Renderer {
 	public:
 		//Methods-------------------------------------
 		static Renderer* Instance() {
 			static Renderer* instance;
 
-			if (!instance) {
+			if (!instance)
 				instance = new Renderer();
-			}
 
 			return instance;
 		}
 
 		int Init(const char* window_title = "Engine", bool fullscreen = false);
-		void InitRenderFBO() {
-			//Init render framebuffer
-			fbo = new FrameBuffer(getWindowSizeX(), getWindowSizeY(), FrameBuffer::FrameType_Depth | FrameBuffer::FrameType_Color, 0, GL_RGB);
-		};
 
 		GLFWwindow* getWindow()const { return m_window; }
 
@@ -65,26 +39,14 @@ namespace Engine
 		int getWindowSizeY()const;
 		float getWindowAspectRatio()const;
 
-		//Does all the render passes
-		void Render();
-
-		void addRenderPass(IRenderPass* renderPass);
-
-		//draws a full screen quad with a texture.
-		//if useFullScreenQuadShader is false will use the previously binded shader (useful for sending shader info for postprocessing effects)
-		static void drawFullScreenQuad(unsigned int textureID, unsigned int textureAtachment = 0, bool useFullScreenQuadShader = true);
-
 		//Variables-----------------------------------
-		GLFWwindow* m_window = nullptr;//Window
-		FrameBuffer* fbo;//Frame buffer object
+		GLFWwindow* m_window;//Window
 
 	private:
 		//Variables-----------------------------------
 		GLFWmonitor* m_primaryMonitor;//Fullscreen monitor
 
-		bool m_fullscreen_mode = false;
-
-		std::vector<IRenderPass*> renderPasses = std::vector<IRenderPass*>();
+		bool m_fullscreen_mode;
 
 		//Methods-------------------------------------
 		Renderer() {
